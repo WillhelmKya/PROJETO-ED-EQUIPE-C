7876e80d8409e920c8a0800728546aeceb42c9df
WillhelmKya <guilherme4479@hotmail.com>
2021-12-12 23:26:55 -0300

    Update Linked List.js

--- src/components/Linked List.js
+++ src/components/Linked List.js
@@ -1,3 +1,11 @@
+function range(start, end) {
+    var ans = [];
+    for (let i = start; i <= end; i++) {
+        ans.push(i);
+    }
+    return ans;
+}
+
 class TADArray {
     constructor(){
         this.a = new Array(1)
@@ -12,19 +20,19 @@ class TADArray {
         return y
     }
     add(i,x){
+        let rr = range(i,this.n)
+        let rr2 = range(i,(this.n)-1)
         /*if (this.n = this.a.length){
             resize()
         }*/
-    
+        this.a[rr] = this.a[rr2]
+        this.a[i] = x
+        this.n++
     }
-}
-function range(start, end) {
-    var ans = [];
-    for (let i = start; i <= end; i++) {
-        ans.push(i);
-    }
-    return ans;
 }
 
-const a = range(1,3)
-console.log(a)
+const a = new TADArray()
+a.add(0,3)
+a.add(1,4)
+a.add(0,5)
+console.log(a.a)
